996a6b3fb4eb5c7c31a6ae96ae2ab13e2ad0f87e
YU <yuchenbao2015@gmail.com>
2023-03-28 23:10:24 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -26,12 +26,27 @@ const auth = getAuth();
 Modal.setAppElement('#root')
 function App() {
   const db = getFirestore(app);
-
+  const [ words, setWords ] = useState([]);
+  const [ clicked, setClicked ] = useState(false)
+  const getWord = async () => { 
+    var today = new Date(),
+    date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    const q = query(collection(db, 'vocabularies'), where('date', '==', date))
+    const querySnapshot = await getDocs(q);
+    const tab=[]
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      tab.push({ ...data, id: doc.id})
+    })
+    console.log(tab);
+    setWords(tab);
+    setClicked(true)
+  }
   const [setting, setSetting] = useState(false);
 
   const Options = (props) => {
     return(
-      <div className='bodyOption'>
+      <button onClick={() => props.f()} className='bodyOption'>
         <div className='bodyOptionLeft'>
           {props.icon}
         </div>
@@ -39,7 +54,7 @@ function App() {
           <p style={{ fontWeight: 'bold', fontSize: '14px' }}>{props.title}</p>
           <p style={{ fontSize: '8px' }}>{props.description}</p>
         </div>
-      </div>
+      </button>
     )
   }
 
@@ -63,6 +78,45 @@ function App() {
     </Modal>
     )
   }
+  const Word = (props) => {
+    return(
+      <div style={{ display: 'flex', flexDirection: 'row', border: '1px black solid', backgroundColor: '#57DDAF', flexGrow: 0.8, marginTop: '3%' , borderRadius: '2%',  }}>
+        <div style={{ width: '40%', borderRight: '1px black solid', display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center', }}>
+          <p>{props.cn}</p>
+          <p>{props.en}</p>
+        </div>
+        <div style={{ flexGrow: 4, backgroundColor: '#D7DDAF' }}>
+          <p>BLABLA</p>
+        </div>
+      </div>
+    )
+  }
+  const VocabulariesBoard = (props) => {
+    return(
+      <Modal
+        isOpen={clicked}
+        className='vocabulariesBoardContents'
+      >
+        <div className='vocabulariesBoard'>
+          {words.map(word => {
+                return(
+                    <Word cn={word.cn} en={word.en} />
+                )
+          })}
+          <div className='vocabulariesButtons' style={{   }}>
+            <button style={{
+              width: '100%',
+              marginTop: '5%',
+            }}
+              onClick={() => setClicked(false)}
+            >
+              <p>返回</p>
+            </button>
+          </div>
+        </div>
+      </Modal>
+    )
+  }
   return (
       <div className="App">
         <header className="App-header">
@@ -73,12 +127,13 @@ function App() {
         </header>
         <body className='App-body'>
             <Options title="今日知识点" description='每天一个托业语法知识点' icon={<BsFillJournalBookmarkFill color="black" size="20px" />} />
-            <Options title="今日单词" description='每天三个单词' icon={<BsClipboard2HeartFill color="black" size="20px" />} />
-            <Options title="检查所学单词" description='检查历史知识点以及单词' icon={<BsClipboardCheckFill size="20px" />}/>
+            <Options f={getWord} title="今日单词" description='每天三个单词' icon={<BsClipboard2HeartFill color="black" size="20px" />} />
+            <Options title="检查历史单词" description='检查历史知识点以及单词' icon={<BsClipboardCheckFill size="20px" />}/>
             <Options title="打卡排行" description='' icon={<GiRank3 size="25px"/>} />
         </body>
         <Setting />
         <SettingContents />
+        <VocabulariesBoard />
       </div>
     );
 }
